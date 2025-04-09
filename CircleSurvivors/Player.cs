eba656dc9cfe129168e263c725539b3ffed7f851
Melwin <23bermel@soltorgsgymnasiet.se>
2025-04-09 22:20:21 +0200

    Player kan nu röra sig runt canvas

--- CircleSurvivors/Player.cs
+++ CircleSurvivors/Player.cs
@@ -12,7 +12,7 @@ namespace CircleSurvivors
         public float x;
         public float y;
         float radius;
-        float movementSpeed = 80;
+        float movementSpeed = 100;
         public Player(int x, int y)
         {
             this.x = x;
@@ -22,6 +22,14 @@ namespace CircleSurvivors
         public void draw()
         {
             Raylib.DrawCircle((int)x, (int)y, radius, Color.Green);
+        }
+        public void update(float deltaTime) //kaller över deltatime som ett argument
+        {
+            //bara if och inte if else för att vi vill att Playern ska kunna gå diagonalt
+            if (Raylib.IsKeyDown(KeyboardKey.W)) y -= movementSpeed * deltaTime;
+            if (Raylib.IsKeyDown(KeyboardKey.S)) y += movementSpeed * deltaTime;
+            if (Raylib.IsKeyDown(KeyboardKey.A)) x -= movementSpeed * deltaTime;
+            if (Raylib.IsKeyDown(KeyboardKey.D)) x += movementSpeed * deltaTime;
         }
     }
 }
--- CircleSurvivors/Program.cs
+++ CircleSurvivors/Program.cs
@@ -9,16 +9,22 @@ namespace CircleSurvivors
             //class initieringar
             Player player = new Player(Config.WindowSizeWidth/2, Config.WindowSizeHeight/2);
 
-            //class initieringar
+            //class initieringar - Variabler
+            float deltaTime;
+
+            //Variabler
             Raylib.InitWindow(Config.WindowSizeWidth, Config.WindowSizeHeight, "Circle Survivors");
             while (!Raylib.WindowShouldClose()) //Game loop
             {
                 Raylib.ClearBackground(Color.White);
+                deltaTime = Raylib.GetFrameTime();
                 Raylib.BeginDrawing();
                 //confines
+                player.update(deltaTime);
                 player.draw();
-
-
+                Raylib.DrawText($"{deltaTime}", 0,0, 32, Color.Black);
+                Raylib.SetTargetFPS(60); //nästan som Thread.sleep(16); men bättre
+                
                 //confines
                 Raylib.EndDrawing();
             }
