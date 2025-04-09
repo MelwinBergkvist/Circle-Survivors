a1dd91c977148bf4ef194d58772583254b454f21
Melwin <23bermel@soltorgsgymnasiet.se>
2025-04-09 21:57:04 +0200

    Skapat ett simpelt Canvas

--- CircleSurvivors/Program.cs
+++ CircleSurvivors/Program.cs
@@ -1,12 +1,28 @@
-﻿using Raylib_cs;
+﻿using Raylib_cs; //Initierar Raylibs library
 
 namespace CircleSurvivors
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Allt som ska ritas as raylibs måste vara inom Begin och End drawing
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(Config.WindowSizeWidth, Config.WindowSizeHeight, "Circle Survivors");
+            while (!Raylib.WindowShouldClose()) //Game loop
+            {
+                Raylib.ClearBackground(Color.White);
+                Raylib.BeginDrawing();
+                //confines
+
+
+
+                //confines
+                Raylib.EndDrawing();
+            }
         }
+    }
+    public static class Config
+    {
+        public static int WindowSizeWidth = 1600;
+        public static int WindowSizeHeight = 800;
     }
 }
