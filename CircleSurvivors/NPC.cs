e683669cdd25143ef17cfa513b572950e9bc1f8e
Melwin Bergkvist <23bermel@soltorgsgymnasiet.se>
2025-04-29 12:37:12 +0200

    tog bort 2 randoms som jag inte behövde, gjorde ny enemies genom en random check i constructorn av NPC.cs, och om den pass så ändrar jag utseendet

--- CircleSurvivors/NPC.cs
+++ CircleSurvivors/NPC.cs
@@ -11,50 +11,60 @@ namespace CircleSurvivors
     {
         public float x;
         public float y;
-        int hitpoints = 100;
-        float radius;
-        float movementSpeed = 80;
         public float spawnImmunity = 0.5f;
         public float sinceSpawn = 0;
+        float movementSpeed = 80;
         float hitCooldown = 0f;
+        float radius = Config.npcRadius;
+        int hitpoints = 100;
+        Color enemyColor = Color.Red;
+        Color enemyHealthColor = Color.Orange;
         public NPC() //constructor
         {
-            this.radius = Config.npcRadius;
-            Random randomSide = new Random();
-            Random randomX = new Random();
-            Random randomY = new Random();
-            int side = randomSide.Next(1, 5); //1 till 5 för att få mellan 1 och 4, random shenanegins
+            Random random = new Random();
+
+            if (random.Next(0,100) > 90 )
+            {
+                hitpoints += 100;
+                radius = 15f;
+                movementSpeed -= 30;
+                enemyColor = Color.DarkGreen;
+                enemyHealthColor = Color.Red;
+                Config.enemyCollisionDamage += 10;
+            }
+
+            int side = random.Next(1, 5); //1 till 5 för att få mellan 1 och 4, random shenanegins
             // 1 = vänster, 2 = höger, 3 = up, 4 = nere
             if (side == 1)
             {
                 this.x = -radius;
-                this.y = randomY.Next(0, Config.WindowSizeHeight);
+                this.y = random.Next(0, Config.WindowSizeHeight);
             }
             else if (side == 2)
             {
                 this.x = Config.WindowSizeWidth + radius;
-                this.y = randomY.Next(0, Config.WindowSizeHeight);
+                this.y = random.Next(0, Config.WindowSizeHeight);
             }
             else if (side == 3)
             {
                 this.y = -radius;
-                this.x = randomX.Next(0, Config.WindowSizeWidth);
+                this.x = random.Next(0, Config.WindowSizeWidth);
             }
             else
             {
                 this.y = Config.WindowSizeHeight + radius;
-                this.x = randomX.Next(0, Config.WindowSizeWidth);
+                this.x = random.Next(0, Config.WindowSizeWidth);
             }
 
         }
         public void draw()
         {
             //Color drawColor = closest ? Color.Pink : Color.Red; //if closest == true, pink, else, red
-            Raylib.DrawCircle((int)x, (int)y, radius, Color.Red); //tar x & y som ints istället för floats
+            Raylib.DrawCircle((int)x, (int)y, radius, enemyColor); //tar x & y som ints istället för floats
 
             //2nd radius som en healthmeter typ
             float healthRadius = radius - (radius * hitpoints / 100f);
-            Raylib.DrawCircle((int)x, (int)y, healthRadius, Color.Orange);
+            Raylib.DrawCircle((int)x, (int)y, healthRadius, enemyHealthColor);
         }
 
         public bool shouldDespawn()
