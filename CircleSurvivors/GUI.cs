c50c7935444af5cac8c747d011111e3d738392fa
Melwin <23bermel@soltorgsgymnasiet.se>
2025-05-09 23:48:47 +0200

    lagt in startscreen i GUI

--- CircleSurvivors/GUI.cs
+++ CircleSurvivors/GUI.cs
@@ -15,6 +15,13 @@ namespace CircleSurvivors
         Color fadeRed, fadeGray, fadeSkyBlue;
         float timeAliveSeconds = 0f;
         float timeAliveMinutes = 0f;
+        float startScreenAlpha = 0f;
+        float fadeInSpeedStart = 100f;
+
+        bool startFadeIn = false;
+        bool hoveredStart;
+        bool clickedStart;
+        bool isTutorialHovered;
 
         public GUI() 
         {
@@ -108,6 +115,74 @@ namespace CircleSurvivors
             {
                 timeAliveSeconds = 0;
                 timeAliveMinutes += 1;
+            }
+        }
+        public void StartScreen(float deltaTime)
+        {
+            if (!Config.startScreen)
+            {
+                Raylib.ClearBackground(Color.Black);
+                Raylib.BeginDrawing();
+
+                int startScreenText = Raylib.MeasureText("Welcome to Circle survivors!", 64);
+                Raylib.DrawText("Welcome to Circle survivors!", Config.WindowSizeWidth / 2 - startScreenText / 2, Config.WindowSizeHeight / 8, 64, Color.White);
+
+                Raylib.DrawRectangle((Config.WindowSizeWidth / 2) - 150, (Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4) - 38, 300, 100, Color.SkyBlue);
+                Rectangle startButton = new Rectangle((Config.WindowSizeWidth / 2) - 150, (Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4) - 38, 300, 100);
+
+                int startScreenBeginText = Raylib.MeasureText("Click here to begin!", 24);
+                Raylib.DrawText("Click here to begin!", Config.WindowSizeWidth / 2 - startScreenBeginText / 2, Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4, 24, Color.DarkBlue);
+
+                Rectangle tutorialButton = new Rectangle(0, Config.WindowSizeHeight / 2, 120, 50);
+                Raylib.DrawRectangle(0, Config.WindowSizeHeight / 2, 120, 50, Color.DarkGray);
+                Raylib.DrawText("How to play?", 10, Config.WindowSizeHeight / 2 + 14, 16, Color.Black);
+
+                isTutorialHovered = Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), tutorialButton);
+                if (isTutorialHovered)
+                {
+                    //ritar om rectangeln i annan färg så man ser att den är hovered
+                    Raylib.DrawRectangle(0, Config.WindowSizeHeight / 2, 120, 50, Color.Gray);
+                    Raylib.DrawText("How to play?", 10, Config.WindowSizeHeight / 2 + 14, 16, Color.Black);
+
+                    //tutorial så man vet vad man ska göra
+                    Raylib.DrawRectangle(Config.WindowSizeWidth / 6 - 10, Config.WindowSizeHeight / 3 - 10, 320, 220, Color.DarkGray);
+                    Raylib.DrawText("Welcome to Circle Survivors!", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3, 16, Color.White);
+                    Raylib.DrawText("You play as the green circle,", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3 + 40, 16, Color.White);
+                    Raylib.DrawText("you move using WASD or direction keys.", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3 + 60, 16, Color.White);
+                    Raylib.DrawText("The game automatically shoots for you.", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3 + 100, 16, Color.White);
+                    Raylib.DrawText("So you just need to stay alive.", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3 + 120, 16, Color.White);
+                    Raylib.DrawText("Enemies spawn in waves,", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3 + 160, 16, Color.White);
+                    Raylib.DrawText("At the end a wave you get a powerup.", Config.WindowSizeWidth / 6, Config.WindowSizeHeight / 3 + 180, 16, Color.White);
+                }
+
+                //checkcollisionpointrec kollar om vektoren getmouseposition är inom rektangel startButton vilken är en kopia av drawrectangle vi gjorde innan
+                //om den är sann och left click är också nedklickad samtidigt så körs inte denna if satsen nåmer, om den inte är sann så skippar allt annat för "continue;"
+                hoveredStart = Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), startButton);
+                clickedStart = Raylib.IsMouseButtonPressed(MouseButton.Left);
+
+                if (hoveredStart) //så man ser att den är hovered
+                {
+                    Raylib.DrawRectangle((Config.WindowSizeWidth / 2) - 150, (Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4) - 38, 300, 100, Color.Blue);
+                    Raylib.DrawText("Click here to begin!", Config.WindowSizeWidth / 2 - startScreenBeginText / 2, Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4, 24, Color.Black);
+                    if (clickedStart)
+                    {
+                        startFadeIn = true; //initierar fade-in
+                    }
+                }
+
+                if (startFadeIn)
+                {
+                    if (startScreenAlpha < 255)
+                    {
+                        startScreenAlpha += fadeInSpeedStart * deltaTime;
+                        if (startScreenAlpha > 255) startScreenAlpha = 255;
+                    }
+                    Color fadeIn = Raylib.Fade(Color.White, startScreenAlpha / 255f);
+                    Raylib.DrawRectangle(0, 0, Config.WindowSizeWidth, Config.WindowSizeHeight, fadeIn);
+                    if (startScreenAlpha == 255) //när fade-in är klar (opaciteten är 100%) så säger vi att startscreenen är klar
+                        Config.startScreen = true;
+                }
+                Raylib.EndDrawing();
             }
         }
     }
--- CircleSurvivors/Program.cs
+++ CircleSurvivors/Program.cs
@@ -30,25 +30,16 @@ namespace CircleSurvivors
 
             //floats
             float deltaTime;
-            float startScreenAlpha = 0f;
-            float fadeInSpeedStart = 100f;
             float waveCooldown = 3.99f; //3.99 så den inte flashar en 4 vid början av cooldownen
             float spawnTime = 1f;
             float collisionCooldown = 1.5f;
             float collisionCooldownTimer = 0f;
 
-            //ints
-            //int enemieSpawnCount = 10 + (Config.wave * Config.wave);
-
             //bools
             bool isPaused = false;
-            bool startScreen = false;
-            bool startFadeIn = false;
             bool noMoreTempSpeed = false;
 
-            bool hoveredStart;
-            bool clickedStart;
-            bool isTutorialHovered;
+            
 
 
             //Colors
@@ -71,70 +62,9 @@ namespace CircleSurvivors
                     continue;
                 }
 
-                if (!startScreen)
+                if (!Config.startScreen)
                 {
-                    Raylib.ClearBackground(Color.Black);
-                    Raylib.BeginDrawing();
-
-                    int startScreenText = Raylib.MeasureText("Welcome to Circle survivors!", 64);
-                    Raylib.DrawText("Welcome to Circle survivors!", Config.WindowSizeWidth/2 - startScreenText / 2,Config.WindowSizeHeight/8, 64, Color.White);
-
-                    Raylib.DrawRectangle((Config.WindowSizeWidth / 2)-150, (Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4)-38, 300, 100, Color.SkyBlue);
-                    Rectangle startButton = new Rectangle((Config.WindowSizeWidth / 2) - 150, (Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4) - 38, 300, 100);
-
-                    int startScreenBeginText = Raylib.MeasureText("Click here to begin!", 24);
-                    Raylib.DrawText("Click here to begin!", Config.WindowSizeWidth / 2 - startScreenBeginText / 2, Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4, 24, Color.DarkBlue);
-
-                    Rectangle tutorialButton = new Rectangle(0,Config.WindowSizeHeight/2, 120, 50);
-                    Raylib.DrawRectangle(0, Config.WindowSizeHeight/2, 120, 50, Color.DarkGray);
-                    Raylib.DrawText("How to play?", 10, Config.WindowSizeHeight/2+14, 16, Color.Black);
-
-                    isTutorialHovered = Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), tutorialButton);
-                    if (isTutorialHovered)
-                    {
-                        //ritar om rectangeln i annan färg så man ser att den är hovered
-                        Raylib.DrawRectangle(0, Config.WindowSizeHeight / 2, 120, 50, Color.Gray);
-                        Raylib.DrawText("How to play?", 10, Config.WindowSizeHeight / 2 + 14, 16, Color.Black);
-                        
-                        //tutorial så man vet vad man ska göra
-                        Raylib.DrawRectangle(Config.WindowSizeWidth/6-10, Config.WindowSizeHeight/3-10, 320, 220, Color.DarkGray);
-                        Raylib.DrawText("Welcome to Circle Survivors!", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3,16,Color.White);
-                        Raylib.DrawText("You play as the green circle,", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3+40,16,Color.White);
-                        Raylib.DrawText("you move using WASD or direction keys.", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3+60,16,Color.White);
-                        Raylib.DrawText("The game automatically shoots for you.", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3+100,16,Color.White);
-                        Raylib.DrawText("So you just need to stay alive.", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3+120,16,Color.White);
-                        Raylib.DrawText("Enemies spawn in waves,", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3+160,16,Color.White);
-                        Raylib.DrawText("At the end a wave you get a powerup.", Config.WindowSizeWidth/6, Config.WindowSizeHeight/3+180,16,Color.White);
-                    }
-
-                    //checkcollisionpointrec kollar om vektoren getmouseposition är inom rektangel startButton vilken är en kopia av drawrectangle vi gjorde innan
-                    //om den är sann och left click är också nedklickad samtidigt så körs inte denna if satsen nåmer, om den inte är sann så skippar allt annat för "continue;"
-                    hoveredStart = Raylib.CheckCollisionPointRec(Raylib.GetMousePosition(), startButton);
-                    clickedStart = Raylib.IsMouseButtonPressed(MouseButton.Left);
-
-                    if (hoveredStart) //så man ser att den är hovered
-                    {
-                        Raylib.DrawRectangle((Config.WindowSizeWidth / 2) - 150, (Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4) - 38, 300, 100, Color.Blue);
-                        Raylib.DrawText("Click here to begin!", Config.WindowSizeWidth / 2 - startScreenBeginText / 2, Config.WindowSizeHeight / 2 + Config.WindowSizeHeight / 4, 24, Color.Black);
-                        if (clickedStart)
-                        {
-                            startFadeIn = true; //initierar fade-in
-                        }
-                    }
-
-                    if (startFadeIn)
-                    {
-                        if (startScreenAlpha < 255)
-                        {
-                            startScreenAlpha += fadeInSpeedStart * deltaTime;
-                            if (startScreenAlpha > 255) startScreenAlpha = 255;
-                        }
-                        Color fadeIn = Raylib.Fade(Color.White, startScreenAlpha / 255f);
-                        Raylib.DrawRectangle(0, 0, Config.WindowSizeWidth, Config.WindowSizeHeight, fadeIn);
-                        if (startScreenAlpha == 255) //när fade-in är klar (opaciteten är 100%) så säger vi att startscreenen är klar
-                            startScreen = true;
-                    }
-                    Raylib.EndDrawing();
+                    gui.StartScreen(deltaTime);
                     continue;
                 }
 
@@ -341,6 +271,7 @@ namespace CircleSurvivors
         public static int killCount = 0;
         public static bool countTime = true;
         public static bool firstWaveAfterRestart = false;
+        public static bool startScreen = false;
 
         //npc,enemy
         public static int npcRadius = 10;
